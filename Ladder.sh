b8f48fb054c2dec6e5d4d36e2c271936b7508988
siddhivinayak08 <siddhivinup@gmail.com>
2020-02-13 12:58:11 +0530

    [imran] added print function

--- Ladder.sh
+++ Ladder.sh
@@ -1,3 +1,14 @@
 echo  "Welcome in Snake And Ladder"
 echo "-----------------------------"
-
+count=100
+declare -a arr[10,10]
+for (( i=0; i<10; i++ ))
+{
+for (( j=0;j<10;j++ ))
+{
+arr[$i,$j]=$count
+echo -ne " ${arr[$i,$j]}"
+count=$(( count-1 ))
+}
+echo ""
+}
